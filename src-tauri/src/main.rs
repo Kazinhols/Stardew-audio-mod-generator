2a414bf8cdcfaf6b99d258986e3b26736ff5d4c6
Kazinhols <kaiocristiano222@gmail.com>
2026-02-11 01:54:53 -0300

    fix: EGL compatibility for Arch-based distros

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        unsafe {
+            std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
+
     stardew_audio_mod_lib::run();
 }
